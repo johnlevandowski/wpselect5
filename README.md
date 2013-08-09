ec6fc1d588217dd6e87370159e9904bafbdd349f
John Levandowski <johntlevandowski@gmail.com>
2013-08-08 19:57:58 -0600

    Customize Post Info, Footer, Post Excerpts and Read More Link

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Github project link: https://github.com/johnlevandowski/wpselect5
 
 === Changelog ===
 
+* Customize Post Info, Footer, Post Excerpts and Read More Link
 * Add YARPP Related Posts
 * Add Adsense Widget Areas
 * Forked from https://github.com/copyblogger/genesis-sample version 2.0.0
--- functions.php
+++ functions.php
@@ -35,3 +35,60 @@ function wpselect_related_posts() {
 		related_posts();
 	}
 }
+
+/**
+ * Customize post info
+ * genesis/lib/structure/post.php
+ */
+add_filter( 'genesis_post_info', 'wpselect_post_info' );
+function wpselect_post_info($post_info) {
+    $post_info = 'Posted on [post_date] [post_edit] [post_comments]';
+    return $post_info;
+}
+
+/**
+ * Customize footer
+ * genesis/lib/structure/footer.php
+ */
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+add_action( 'genesis_footer', 'wpselect_do_footer' );
+function wpselect_do_footer() {
+    ?><p>&copy; Copyright 2010-<?php echo date( 'Y' ); ?> <a title="wpselect.com" href="http://wpselect.com/">wpselect.com</a></p>
+	<p>Powered by <a title="Genesis Framework" href="http://wpselect.com/go/genesis/">Genesis</a>, 
+	<a title="Hosting by HostGator" href="http://wpselect.com/go/hostgator/">HostGator</a>, 
+	and <a title="WordPress" href="http://wordpress.org/">WordPress</a></p>
+	<p><a title="Privacy Policy" href="http://wpselect.com/privacy-policy/">Privacy Policy</a> &middot; 
+	<a title="Disclaimer" href="http://wpselect.com/disclaimer/">Disclaimer</a> &middot; 
+	<a title="FTC Disclosure" href="http://wpselect.com/ftc-disclosure/">FTC Disclosure</a> &middot; 
+	<a title="Image Attribution" href="http://wpselect.com/image-attribution/">Image Attribution</a></p>
+    <?php
+}
+
+/**
+ * Customize length of post excerpts
+ * default is 55
+ */
+add_filter( 'excerpt_length', 'wpselect_excerpt_length' );
+function wpselect_excerpt_length($length) {
+    return 60;
+}
+
+/** Customize more link of post excerpts */
+add_filter('excerpt_more', 'wpselect_excerpt_more');
+function wpselect_excerpt_more($more) {
+	return ' &hellip;';
+}
+
+/** Customize jpeg quality */
+add_filter( 'jpeg_quality', 'wpselect_jpeg_quality' );
+function wpselect_jpeg_quality($quality) {
+	return (int)79;
+}
+
+/** Add read more link to post on all archive pages */
+add_action( 'genesis_entry_content', 'wpselect_read_more_post_content', 15 );
+function wpselect_read_more_post_content() {
+	if ( ! is_singular() ) {
+	echo '<p class="wpselect-read-more"><a href="' . get_permalink() . '">Continue Reading &rarr;</a></p>';
+	}
+}
