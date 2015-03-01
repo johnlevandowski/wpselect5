5a2aacdd5adc73e9924ccc43859fb1e19dd54e7d
John Levandowski <john@johnlevandowski.com>
2015-03-01 08:30:02 -0700

    YARPP google analytics tracking code

--- yarpp-template-wpselect.php
+++ yarpp-template-wpselect.php
@@ -9,7 +9,7 @@ if (have_posts()): ?>
 <h3>Related Posts</h3>
 <ul>
 	<?php while (have_posts()) : the_post(); ?>
-	<li><a href="<?php the_permalink() ?>" onclick="javascript:_gaq.push(['_trackEvent','related-post-click','yarpp']);" rel="bookmark"><?php the_title(); ?></a><!-- (<?php the_score(); ?>)--></li>
+	<li><a href="<?php the_permalink() ?>" onclick="__gaTracker('send', 'event', 'related-post-click', 'yarpp', '<?php the_permalink() ?>');" rel="bookmark"><?php the_title(); ?></a><!-- (<?php the_score(); ?>)--></li>
 	<?php endwhile; ?>
 </ul>
 </div>
