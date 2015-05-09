ee57f1736ab0575f1508c8ed41196b5acd0775a4
John Levandowski <john@johnlevandowski.com>
2015-05-09 02:36:19 -0600

    Search widget ADA compliant

--- lib/google-search.php
+++ lib/google-search.php
@@ -1,14 +1,5 @@
 <?php
 /**
- * Customize the search text
- * genesis/lib/structure/search.php
- */
-add_filter( 'genesis_search_text', 'wpselect_search_text');
-function wpselect_search_text() {
-	return esc_attr__( 'Google Search ...', 'genesis' );
-}
-
-/**
  * Customize the search form
  * genesis/lib/structure/search.php
  */
@@ -16,8 +7,8 @@ add_filter( 'genesis_search_form', 'wpselect_search_form', 10, 4);
 function wpselect_search_form( $form, $search_text, $button_text, $label ) {
 	$form = '
 		<form method="get" class="search-form" action="' . home_url() . '/google-cse/" role="search">
-			' . $label . '
-			<input type="search" name="q" placeholder="' . esc_attr( $search_text ) . '" />
+			<label for="google-search" class="screen-reader-text">Google Search</label>
+			<input type="search" name="q" id="google-search" placeholder="Google Search ..." />
 			<input type="submit" value="' . esc_attr( $button_text ) . '" />
 		</form>
 	';
